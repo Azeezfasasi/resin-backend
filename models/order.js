a56c9953b7b61d3c6af3c7fbf4bb577e96e7f79a
azeezfasasi <fas.azeez@gmail.com>
2025-03-21 22:52:33 +0100

    updated

--- models/order.js
+++ models/order.js
@@ -1,24 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const orderSchema = new mongoose.Schema({
-//     orderNumber: { type: Number, required: true },
-//     orderDate: { type: String, required: true },
-//     productName: { type: String, required: true },
-//     amount: { type: Number, required: true },
-//     orderStatus: { type: String, enum: [
-//         'Pending', 'Processing', 'On Hold', 'Confirmed', 'Packing',
-//         'Out for Delivery', 'Canceled', 'Delivered', 'Completed', 'Refunded',
-//         'Partially Shipped'
-//     ], default: 'Pending' },
-//     // Add other fields as needed (e.g., userId, shippingAddress, etc.)
-// }, {
-//     timestamps: true, // Adds createdAt and updatedAt fields
-// });
-
-// const Order = mongoose.model('Order', orderSchema);
-
-// module.exports = Order;
-
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
@@ -31,10 +10,10 @@ const orderSchema = new mongoose.Schema({
     paymentMethod: { type: String, required: true },
     firstName: { type: String, required: true},
     lastName: { type: String, required: true},
-    country: { type: String, required: true},
+    country: { type: String},
     streetAddress: { type: String, required: true},
     townCity: { type: String, required: true},
-    state: { type: String, required: true},
+    state: { type: String},
     phone: { type: String, required: true},
     email: { type: String, required: true},
     orderStatus: {
