04772a74be2b6235e4e87cde4ada7c3f19865113
azeezfasasi <fas.azeez@gmail.com>
2025-03-23 15:05:11 +0100

    check

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -233,18 +233,29 @@ const getAllUsers = async (req, res) => {
 };
 
 // Get me (Current User)
+// const getMe = async (req, res) => {
+//   try {
+//     console.log("getMe called, req.user:", req.user);
+//       if (!req.user) {
+//           return res.status(401).json({ message: "Unauthorized" });
+//       }
+//       const user = await User.findById(req.user._id);
+//       console.log("User found:", user);
+//       if (!user) {
+//           return res.status(404).json({ message: "User not found" });
+//       }
+//       res.status(200).json(user);
+//   } catch (error) {
+//       console.error("Error fetching user details:", error);
+//       res.status(500).json({ message: "Internal server error" });
+//   }
+// };
 const getMe = async (req, res) => {
   try {
-    console.log("getMe called, req.user:", req.user);
-      if (!req.user) {
+      if (!req.user || !req.user.dbUser) {
           return res.status(401).json({ message: "Unauthorized" });
       }
-      const user = await User.findById(req.user._id);
-      console.log("User found:", user);
-      if (!user) {
-          return res.status(404).json({ message: "User not found" });
-      }
-      res.status(200).json(user);
+      res.status(200).json(req.user.dbUser); 
   } catch (error) {
       console.error("Error fetching user details:", error);
       res.status(500).json({ message: "Internal server error" });
