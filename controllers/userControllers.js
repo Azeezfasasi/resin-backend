c0da004ccd3305911b73e346be96514e720b08a7
azeezfasasi <fas.azeez@gmail.com>
2025-03-23 14:03:22 +0100

    update

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -238,7 +238,7 @@ const getMe = async (req, res) => {
       if (!req.user) {
           return res.status(401).json({ message: "Unauthorized" });
       }
-      const user = await User.findById(req.user._id);
+      const user = await User.findById(req.user.id);
       if (!user) {
           return res.status(404).json({ message: "User not found" });
       }
