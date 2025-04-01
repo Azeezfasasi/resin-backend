30667769b08d5b07bfcdcd0b93b536317fcf9a49
azeezfasasi <fas.azeez@gmail.com>
2025-04-01 09:22:49 +0100

    updated revert

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,20 +12,9 @@ const {
 } = require("../controllers/productControllers");
 const multer = require("multer");
 
-const storage = multer.memoryStorage(); // Store files in memory
-const uploadMulter = multer({ storage: storage });
-
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
-// router.post("/", upload.array("images", 10), createProduct); 
-router.post(
-    "/",
-    uploadMulter.fields([
-        { name: "images", maxCount: 10 },
-        // dynamically made variant image names.
-    ]),
-    createProduct
-);
+router.post("/", upload.array("images", 10), createProduct); 
 router.delete("/:id", deleteProduct);
 router.get('/count', async (req, res) => {
     try {
